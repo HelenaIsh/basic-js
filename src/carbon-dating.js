ea254ed730797faa49f75d33f1083e2046121f41
HelenaIsh <elena-ishmatova@mail.ru>
2021-09-13 15:22:38 +0500

    implement first three functions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(isNaN(sampleActivity) || sampleActivity <= 0 || (typeof sampleActivity !== "string")  || sampleActivity > 15) {
+    return false;
+  }
+  const k = 0.693 /HALF_LIFE_PERIOD;
+  const a = Math.log(MODERN_ACTIVITY/sampleActivity)
+  return Math.ceil(a/k);
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  return members.filter(value => typeof value === "string")
+    .map(value => value.toUpperCase().trim())
+    .sort()
+    .map(value => value[0])
+    .join('');
 }
