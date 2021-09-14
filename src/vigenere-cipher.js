ea2d8cc68bc64d3092e4d0dcfcbec21d2e1114e0
HelenaIsh <elena-ishmatova@mail.ru>
2021-09-14 15:34:18 +0500

    feat: implement VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,47 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return vizhener(message, key, true, this.isDirect);
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return vizhener(encryptedMessage, key, false, this.isDirect);
+  }
+}
+function vizhener(message, key, encrypt, isDirect){
+  message = message.toUpperCase();
+  key = key.toUpperCase();
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = '';
+  for(let i = 0, j = 0; i < message.length; i++, j++){
+    let messageIndex = alphabet.indexOf(message[i]);
+    let keyIndex = alphabet.indexOf(key[((j >= key.length) ? j%key.length : j)]);
+    let alphabetIndex;
+    if (messageIndex !== -1) {
+      if (encrypt) {
+        alphabetIndex = (alphabet.length + (messageIndex + keyIndex)) % alphabet.length;
+      } else {
+        alphabetIndex = (alphabet.length + (messageIndex - keyIndex)) % alphabet.length;
+      }
+      let symbol = alphabet[alphabetIndex];
+      result += symbol;
+    } else {
+      j--;
+      result += message[((i >= message.length) ? i%message.length : i)];
+    }
+  }
+  if (isDirect) {
+    return result;
+  } else {
+    return result.split('').reverse().join('');
   }
 }
